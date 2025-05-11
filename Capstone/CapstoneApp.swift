b93f4ee5b8152dfec6e5b0be0147e0273533ada5
Jessica Cooper <cooperjessica96@gmail.com>
2025-05-11 11:11:12 -0400

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Jessica Cooper on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
